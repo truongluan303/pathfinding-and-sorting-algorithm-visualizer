a977f2cdcd0ebd5f7ae448778030c4a37609b365
Hoang Phuc Luan Truong <83048295+truongluan303@users.noreply.github.com>
2021-08-02 07:27:45 -0700

    Update visualizer.py

--- visualizer.py
+++ visualizer.py
@@ -4,14 +4,16 @@ import pygame
 ### THE PARENT CLASS FOR THE TWO VISUALIZER ###
 #---------------------------------------------#
 class Visualizer:
-    def __init__(self, width, height, title, color) -> None:
+    def __init__(self, width, height, title, color=(0,0,0), icon=None) -> None:
         # initialize pygame
         pygame.init()               
         # initialize the display
         self.screen = pygame.display.set_mode((width, height))
         self.screen.fill(color)
-        pygame.display.set_caption(title)
         self.clock = pygame.time.Clock()
+        pygame.display.set_caption(title)
+        if icon is not None:
+            pygame.display.set_icon(icon)
         
 
     def draw(self):

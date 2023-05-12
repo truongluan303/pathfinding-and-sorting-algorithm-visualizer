c2c4d161c31fc1a326965d6c1a43198fece1386a
Luan Truong <truongluan303@gmail.com>
2023-05-12 07:05:42 -0700

    Code refactor

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/python3
 
 from src.home_window import HomeWindow
@@ -5,3 +6,13 @@ from src.home_window import HomeWindow
 if __name__ == "__main__":
     while True:
         HomeWindow()
+=======
+#!/usr/bin/python
+
+from src.home_window import HomeWindow
+
+
+if __name__ == "__main__":
+    while True:
+        HomeWindow()
+>>>>>>> Code refactor
